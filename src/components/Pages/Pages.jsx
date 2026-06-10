7e13ca459e95d75d284a63e59dfe49134625add3
Aayush Trivedi <aayush.t200@gmail.com>
2026-06-10 13:27:40 +0530

    feat: simplify ToggleTheme component usage in Dashboard and Pages; replace Button with native button element

--- src/components/Pages/Pages.jsx
+++ src/components/Pages/Pages.jsx
@@ -1,4 +1,5 @@
 import Button from '../Button/Button';
+import ToggleTheme from '../ThemeToggleComponent/ToggleTheme';
 const Pages = ({
       children,
       className = '',
@@ -11,8 +12,10 @@ const Pages = ({
       // if (children == null) return null;
       return (
             <div className={`page ${className} flex h-dvh gap-2 p-1 md:p-4`}>
+                  <ToggleTheme />
+
                   <div className="page-left flex w-1/13 flex-col items-center md:w-1/12">
-                        <div className="back-button flex ">
+                        <div className="back-button flex">
                               <button
                                     className={`page-button text-xl md:text-3xl lg:text-4xl hover:${hover} cursor-pointer duration-150 ease-in-out hover:scale-125`}
                                     onClick={onClick}
--- src/components/ThemeToggleComponent/ToggleTheme.jsx
+++ src/components/ThemeToggleComponent/ToggleTheme.jsx
@@ -1,13 +1,15 @@
-import Button from '../Button/Button';
 import { useContext } from 'react';
 import { DataContext } from '../../context/DataContext';
 const ToggleTheme = ({ className }) => {
       const { theme, toggleTheme } = useContext(DataContext);
 
       return (
-            <Button onClick={toggleTheme} className={`${className}`}>
+            <button
+                  onClick={toggleTheme}
+                  className={`${className} absolute right-1 z-10 cursor-pointer p-5 hover:scale-125`}
+            >
                   {theme === 'dark' ? '🌙' : '☀️'}
-            </Button>
+            </button>
       );
 };
 
